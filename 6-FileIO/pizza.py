01feb02f56c30b63204e473cd8485020ce1dd2e8
Raynard-W <raynard.widago@education.nsw.gov.au>
2026-04-26 09:29:07 +0000

    6.2

--- 6-FileIO/pizza.py
+++ 6-FileIO/pizza.py
@@ -1,1 +1,32 @@
-import sys.
+import sys
+import csv
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit
+    if len(sys.argv) > 2:
+        sys.exit
+
+
+filename = sys.argv[1]
+
+if not filename.endswith(".csv"):
+    sys.exit("Not a csv file")
+
+
+def read_csv(filename):
+    try:
+        with open(filename) as file:
+            return
+    except ValueError:
+        sys.exit("file was not found")
+
+
+def print_table(rows):
+    print(rows)
+    pass
+
+
+if __name__ == "__main__":
+    main()
